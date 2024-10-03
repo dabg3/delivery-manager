93003b02592e68d8a006f9897b3eb1e28a35013b
Davide Bigotti <bigotti.davide@hotmail.com>
2024-10-03 18:34:45 +0200

    wip

--- src/main/java/org/example/location/GeoPoint.java
+++ src/main/java/org/example/location/GeoPoint.java
@@ -18,6 +18,14 @@ public class GeoPoint {
     @Column(nullable = false)
     private double longitude;
 
+    public GeoPoint() {
+    }
+
+    public GeoPoint(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
     public double getLatitude() {
         return latitude;
     }
@@ -32,5 +40,12 @@ public class GeoPoint {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public static GeoPoint random() {
+        double f = Math.random() / Math.nextDown(1.0);
+        double lat = -90 * (1.0 - f) + 90 * f;      // [-90..90]
+        double lon = -180 * (1.0 - f) + 180 * f;    // [-180..180]
+        return new GeoPoint(lat, lon);
     }
 }
